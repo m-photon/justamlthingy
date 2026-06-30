20e11b937aefae86422ccddc4c1d9647289e8e11
m-photon <averagejellym.m@gmail.com>
2026-06-30 15:44:00 -0500

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const C='kl-v4';self.addEventListener('install',()=>self.skipWaiting());self.addEventListener('activate',()=>self.clients.claim());self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
